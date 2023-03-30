53ad268b6f785705a63ca8b65ff3457c71e7114c
osirisdavid <davidosirisayala@gmail.com>
2023-03-30 17:40:30 -0300

    init project

---
+++ TP-1/ArbolBinario.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: osiri
+ * Fecha: 30/3/2023
+ * Hora: 17:31
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace TP_1
+{
+	public class ArbolBinario<T>
+	{
+		
+		private T dato;
+		private ArbolBinario<T> hijoIzquierdo;
+		private ArbolBinario<T> hijoDerecho;
+	
+		
+		public ArbolBinario(T dato) {
+			this.dato = dato;
+		}
+	
+		public T getDatoRaiz() {
+			return this.dato;
+		}
+	
+		public ArbolBinario<T> getHijoIzquierdo() {
+			return this.hijoIzquierdo;
+		}
+	
+		public ArbolBinario<T> getHijoDerecho() {
+			return this.hijoDerecho;
+		}
+	
+		public void agregarHijoIzquierdo(ArbolBinario<T> hijo) {
+			this.hijoIzquierdo=hijo;
+		}
+	
+		public void agregarHijoDerecho(ArbolBinario<T> hijo) {
+			this.hijoDerecho=hijo;
+		}
+	
+		public void eliminarHijoIzquierdo() {
+			this.hijoIzquierdo=null;
+		}
+	
+		public void eliminarHijoDerecho() {
+			this.hijoDerecho=null;
+		}
+	
+		public bool esHoja() {
+			return this.hijoIzquierdo==null && this.hijoDerecho==null;
+		}
+		
+		public void inorden() {
+		}
+		
+		public void preorden() {
+		}
+		
+		public void postorden() {
+		}
+		
+		public void recorridoPorNiveles() {
+		}
+	
+		public int contarHojas() {
+			return 0;
+		}
+		
+		public void recorridoEntreNiveles(int n,int m) {
+		}
+	}
+
+}
