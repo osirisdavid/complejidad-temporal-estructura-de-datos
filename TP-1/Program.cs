d142fff374a1ffc96f3646306e99608299abc5b8
osirisdavid <davidosirisayala@gmail.com>
2023-03-30 17:22:28 -0300

    init project

---
+++ TP-1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: osiri
+ * Fecha: 30/3/2023
+ * Hora: 17:19
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace TP_1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
