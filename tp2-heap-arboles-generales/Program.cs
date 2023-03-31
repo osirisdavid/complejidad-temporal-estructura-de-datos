dd33f4dfb7f35ff9bf3e3ee69ff8302de4205831
osirisdavid <davidosirisayala@gmail.com>
2023-03-30 21:45:39 -0300

    init practica 2

---
+++ tp2-heap-arboles-generales/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: osiri
+ * Fecha: 30/3/2023
+ * Hora: 21:44
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace tp2_heap_arboles_generales
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            // TODO: Implement Functionality Here
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
